aca82cd6cca2c8c5b7aef6688c5fa388cac3672f
Jahanzaib Khan Durrani <jahanzaib.khan.offsec@gmail.com>
2017-05-03 23:43:28 +0500

    initial commit

---
+++ LED_BRIGHTNESS/examples/Example1/Example1.ino
@@ -0,0 +1,34 @@
+#include <LED_BRIGHTNESS.h>
+
+LED_BRIGHTNESS LED(3, 1); // any pin with PWM capability can be used 
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  for(int i=0;i<=255;i++){
+    LED.make_led_brighter();
+    delay(10);
+  }
+
+  
+  for(int i=0;i<=255;i++){
+    LED.make_led_dimmer();
+    delay(10);
+  }
+  
+  /*
+  
+	void make_led_brighter();
+	void make_led_max_bright();
+    void make_led_dimmer();
+    void make_led_max_dim();
+	void set_rate(int rate);
+    int set_brightness_percent(int percent);
+  
+  */
+
+}
